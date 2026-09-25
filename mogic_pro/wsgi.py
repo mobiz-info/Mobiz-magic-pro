d6b1442957336e1eec87bdb27190a2a72ca1f86e
Muhammed Anshid <anshianshid58@gmail.com>
2026-09-25 13:26:07 +0530

    Fix DJANGO_SETTINGS_MODULE to mogic_pro.settings

--- mogic_pro/wsgi.py
+++ mogic_pro/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parishudam_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mogic_pro.settings')
 
 application = get_wsgi_application()
