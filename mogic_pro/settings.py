3adf9e841a843704e47fc15160e8fb1dc81060d1
Muhammed Anshid <anshianshid58@gmail.com>
2026-09-25 11:42:45 +0530

    db changed

--- mogic_pro/settings.py
+++ mogic_pro/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'mogic_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        
     }
 }
 
